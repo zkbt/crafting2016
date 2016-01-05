62f256e6d2ce73afea0b7c9949b3ad9f5991e32a
Zach Berta-Thompson <zkbt@mit.edu>
2016-01-05 09:16:28 -0500

    add '-a, --auto' flag for skipping the alignment step

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # craftroom
 Various tools for crafting the cosmos!
+
+Started by Zach Berta-Thompson (zkbt@mit.edu), January 2016.
--- stereo.py
+++ stereo.py
@@ -1,104 +1,104 @@
 #!/usr/bin/env python
 
-import os, sys, warnings, numpy as np, matplotlib.pyplot as plt
+import os, sys, warnings, optparse, numpy as np, matplotlib.pyplot as plt
 warnings.filterwarnings("ignore")
 from PIL import Image
 
 
 class iplot():
-	def __init__(self, nRows, nCols, verbose=True, **kwargs):
-		# input number of rows and number of columns
-		# keep track of messages
-		self.verbose = verbose
-		self.prefix = '   [i] '
-		if self.verbose:
-			print self.prefix + 'creating an interactive figure'
-			self.indent()
+    def __init__(self, nRows, nCols, verbose=True, **kwargs):
+        # input number of rows and number of columns
+        # keep track of messages
+        self.verbose = verbose
+        self.prefix = '   [i] '
+        if self.verbose:
+            print self.prefix + 'creating an interactive figure'
+            self.indent()
 
-		# create a gridspec object
-		self.gs = plt.matplotlib.gridspec.GridSpec(nRows,nCols,**kwargs)
-		self.figure = plt.gcf()
+        # create a gridspec object
+        self.gs = plt.matplotlib.gridspec.GridSpec(nRows,nCols,**kwargs)
+        self.figure = plt.gcf()
+        self.figure.set_facecolor('white')
+        # create an empty dictionary to store axes
+        self.axes = {}
 
-		# create an empty dictionary to store axes
-		self.axes = {}
+        plt.show(block=False)
 
-		plt.show(block=False)
+    def indent(self):
+        self.prefix = ' ' + self.prefix
 
-	def indent(self):
-		self.prefix = ' ' + self.prefix
+    def unindent(self):
+        self.prefix = self.prefix[1:]
 
-	def unindent(self):
-		self.prefix = self.prefix[1:]
+    def subplot(self, row=0, col=0,rowspan=1,colspan=1,name=None, **kwargs):
+        # create the axis object, using the gridspec language
+        #    for example,
 
-	def subplot(self, row=0, col=0,rowspan=1,colspan=1,name=None, **kwargs):
-		# create the axis object, using the gridspec language
-		#	for example,
+        ax = plt.subplot(self.gs[row:row + rowspan, col:col + colspan], **kwargs)
+        if name == None:
+            name = 'ax{0}'.format(len(self.axes))
+        self.axes[name] = ax
+        if self.verbose:
+            print self.prefix + 'added axes at rows {0}-{1} and columns {2}-{3}, named {4}'.format(row,row + rowspan, col,col + colspan,name)
+        return ax
 
-		ax = plt.subplot(self.gs[row:row + rowspan, col:col + colspan], **kwargs)
-		if name == None:
-			name = 'ax{0}'.format(len(self.axes))
-		self.axes[name] = ax
-		if self.verbose:
-			print self.prefix + 'added axes at rows {0}-{1} and columns {2}-{3}, named {4}'.format(row,row + rowspan, col,col + colspan,name)
-		return ax
+    def connect(self):
+        # connect all the event-handling functions
+        #self.figure.canvas.mpl_connect('axes_enter_event', self.onEnterAxes)
+        #self.figure.canvas.mpl_connect('axes_leave_event', self.onLeaveAxes)
+        self.cids = []
+        self.cids.append(self.figure.canvas.mpl_connect('button_release_event', self.onClick))
 
-	def connect(self):
-		# connect all the event-handling functions
-		#self.figure.canvas.mpl_connect('axes_enter_event', self.onEnterAxes)
-		#self.figure.canvas.mpl_connect('axes_leave_event', self.onLeaveAxes)
-		self.cids = []
-		self.cids.append(self.figure.canvas.mpl_connect('button_release_event', self.onClick))
+    def onEnterAxes(self, event):
+        print 'enter_axes', event.inaxes
+        event.inaxes.patch.set_facecolor('yellow')
+        event.canvas.draw()
 
-	def onEnterAxes(self, event):
-		print 'enter_axes', event.inaxes
-		event.inaxes.patch.set_facecolor('yellow')
-		event.canvas.draw()
+    def onLeaveAxes(self, event):
+        print 'leave_axes', event.inaxes
+        event.inaxes.patch.set_facecolor('white')
+        event.canvas.draw()
 
-	def onLeaveAxes(self, event):
-		print 'leave_axes', event.inaxes
-		event.inaxes.patch.set_facecolor('white')
-		event.canvas.draw()
-
-	def onClick(self, event):
-		if event.xdata is None:
-			print self.prefix + " that wasn't a very nice click. Could you please try again?"
-			return
-		if self.verbose:
-			print self.prefix + ' clicked at {0}, {1}'.format(event.xdata, event.ydata)
-		self.lastMouseClick = event
+    def onClick(self, event):
+        if event.xdata is None:
+            print self.prefix + " that wasn't a very nice click. Could you please try again?"
+            return
+        if self.verbose:
+            print self.prefix + ' clicked at {0}, {1}'.format(event.xdata, event.ydata)
+        self.lastMouseClick = event
 
 
-		self.mouseClicks.append(event)
-		self.remainingClicks -= 1
-		if self.remainingClicks <= 0:
-			self.stop()
+        self.mouseClicks.append(event)
+        self.remainingClicks -= 1
+        if self.remainingClicks <= 0:
+            self.stop()
 
-	def getMouseClicks(self, n=2):
-		# say what's happening
-		if self.verbose:
-			print self.prefix + "waiting for {0} mouse clicks.".format(n)
-			self.indent()
+    def getMouseClicks(self, n=2):
+        # say what's happening
+        if self.verbose:
+            print self.prefix + "waiting for {0} mouse clicks.".format(n)
+            self.indent()
 
-		# set the countdown for the number of clicks
-		self.remainingClicks = n
-		self.mouseClicks = []
+        # set the countdown for the number of clicks
+        self.remainingClicks = n
+        self.mouseClicks = []
 
-		# start the event handling loop
-		self.start()
+        # start the event handling loop
+        self.start()
 
-		self.unindent()
-		# return a list of mouse clicks, which the loop should have generated
-		return self.mouseClicks
+        self.unindent()
+        # return a list of mouse clicks, which the loop should have generated
+        return self.mouseClicks
 
-	def start(self):
-		self.connect()
-		plt.draw()
-		self.figure.canvas.start_event_loop(0)
+    def start(self):
+        self.connect()
+        plt.draw()
+        self.figure.canvas.start_event_loop(0)
 
-	def stop(self):
-		self.figure.canvas.stop_event_loop()
-		for cid in self.cids:
-			self.figure.canvas.mpl_disconnect(cid)
+    def stop(self):
+        self.figure.canvas.stop_event_loop()
+        for cid in self.cids:
+            self.figure.canvas.mpl_disconnect(cid)
 
 
 class Stereo(object):
@@ -149,7 +149,7 @@ class Stereo(object):
                 if self.iplot.axes[k] == click.inaxes:
                     whicheye = k
                     self.referencepoints[k] = np.round(np.array([click.xdata, click.ydata])).astype(np.int)
-            print " You clicked the {0} image at {1}".format(whicheye, self.referencepoints[whicheye])
+            print "  You clicked the {0} image at {1}".format(whicheye, self.referencepoints[whicheye])
             #self.iplot.axes[whicheye].scatter(*self.referencepoints[whicheye], s=50)
         nudgex, nudgey = self.referencepoints['right']-self.referencepoints['left']
 
@@ -179,6 +179,7 @@ class Stereo(object):
         while(os.path.exists(self.filename(i))):
             i += 1
         self.combined.save(self.filename(i))
+        print "   ...saved to {0}".format(self.filename(i))
 
 class SideBySide(Stereo):
     '''A stereoscopic image object, where images are shown side-by-side.'''
@@ -234,9 +235,41 @@ def nextfilename(guess):
 
 #s = Stereo('couchtest/left.jpg','couchtest/right.jpg')
 if __name__ == '__main__':
-    s = Stereo(sys.argv[1], sys.argv[2])
-    s.adjust()
+
+    parser = optparse.OptionParser(usage = """
+    ./stereo.py [options] left_image right_image
+
+Options:
+    -a, --auto = skip the alignment step""")
+
+    # figure out which options have been set, and the image filenames
+    parser.add_option('-a', '--auto',
+        action = 'store_const', const = True, dest = 'automatic', default=False,
+        help = 'skip the adjustment step (this assumes your images are perfectly aligned)')
+    options, args = parser.parse_args()
+
+    # complain if the wrong number of filenames is included
+    if len(args) != 2:
+        parser.error("""Hmmm...there are {0} arguments to ./stereo.py.
+                        Please run with two image filenames.
+                        (e.g. "./stereo.py leftimage.jpg rightimage.jpg")
+                        """.format(len(args)))
+
+    print options.automatic
+
+
+    # create the stereo image
+    s = Stereo(*args)
+
+
+    # unless the "--auto" option is set,
+    if options.automatic == False:
+        s.adjust()
+
+    # create a side-by-side image
     sbs = SideBySide(s)
     sbs.display()
+
+    # create a blue-red image
     a = Anaglyph(s)
     a.display()
